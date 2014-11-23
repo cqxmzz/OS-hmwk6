72235a4f1b94d17b7128585f2b9e10a816278c60
W4118 Student <w4118@w4118>
2014-11-23 12:11:49 -0500

    part 1 all set, start test

--- flo-kernel/kernel/gps.c
+++ flo-kernel/kernel/gps.c
@@ -3,8 +3,26 @@
 #include <linux/syscalls.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
-
+#include <linux/gps.h>
+/* Wendan Kang*/
+static DEFINE_SPINLOCK(k_loc_lock);
+static struct gps_location k_gps_location = {
+	.latitude = 0.0,
+	.longitude = 0.0,
+	.accuracy = 0.0
+};
+/* citation: hmwk3 solution*/
 SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc)
 {
+	struct gps_location k_loc;
+	if(current_uid() != 0)
+		return -EACCES;
+	if(loc == NULL)
+		return -EINVAL;
+	if(copy_from_user(&k_loc, loc, sizeof(k_loc)))
+		return -EFAULT;
+	spin_lock(&k_loc_lock);
+	memcpy(&k_gps_location, &k_loc, sizeof(k_loc));
+	spin_unlock(&k_loc_lock);
 	return 0;
 }
