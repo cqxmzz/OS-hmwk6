7e997a7e175da7aeb02fb8cd91857f3e65dd9a47
to0 <yuanancai@gmail.com>
2014-11-26 02:19:16 -0500

    q3 structure

--- flo-kernel/kernel/gps.c
+++ flo-kernel/kernel/gps.c
@@ -26,3 +26,48 @@ SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc)
 	spin_unlock(&k_loc_lock);
 	return 0;
 }
+
+/* Cai Yuannan */
+SYSCALL_DEFINE2(get_gps_location, const char __user *, pathname,
+struct gps_location __user *, loc)
+{
+	struct gps_location kloc;
+	struct inode *inode;
+	struct path path;
+	int coord_age = 0;
+
+	if (pathname == NULL || loc == NULL)
+		return -EINVAL;
+
+	if (!capable(CAP_SYS_ADMIN)) {
+		pr_err("NOT A SYS ADMIN!!\n");
+		return -EACCES;
+	}
+
+	if (user_path(pathname, &path)) {
+		return -EFAULT;
+	}
+
+	if (!path.dentry->d_inode)
+		return -EFAULT;
+
+	inode = path.dentry->d_inode;
+
+	if (inode->i_op && inode->i_op->get_gps_location)
+		coord_age = inode->i_op->get_gps_location(inode, &kloc);
+	else
+	return -EFAULT;
+
+	if (copy_to_user(&loc->latitude, &kloc.latitude, sizeof(loc->latitude)))
+		return -EFAULT;
+
+	if (copy_to_user(&loc->longitude, &kloc.longitude,
+		sizeof(loc->longitude)))
+		return -EFAULT;
+	
+	if (copy_to_user(&loc->accuracy, &kloc.accuracy,
+		sizeof(loc->accuracy)))
+		return -EFAULT;
+
+	return coord_age;
+}
